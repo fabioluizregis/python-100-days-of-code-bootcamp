270bdf8674602fc47b2b52292b6a2964000a5358
Fabio Luiz Régis <88636618+fabioluizregis@users.noreply.github.com>
2024-08-30 11:35:04 +0000

    continuaçao da aula 2

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -27,3 +27,26 @@ print("")
 print(True)
 print(False)
 
+# Showing the type of a data
+print()
+print(type("Text"))
+print(type(1234))
+print(type(True))
+print(type(3.14))
+
+# Type conversion ( works with int() , float(), str(), bool() )
+print()
+print(int("100")+int("100"))
+
+print()
+print("Number of letters in your name: " + str(len(input("Enter your name: "))))
+
+# Mathematical Operations
+print()
+print(123 + 456)
+print(7 - 5)
+print(3 * 2)
+print(5 / 3)
+print(5 // 3) # Eliminates the values after .
+print(2 ** 3) # Potenciation
+
